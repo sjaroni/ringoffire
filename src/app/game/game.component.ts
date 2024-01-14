f09124a38f1e96ba9c0a81a71cbdd2697bf6e7bf
Stefan Jaroni <sjaroni@outlook.de>
2024-01-14 22:26:20 +0100

    add pick card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  pickCardAnimation = false;
 
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
