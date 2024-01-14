bcd54e0386d6251857c53328fbc5b1a0eb8badd3
Stefan Jaroni <sjaroni@outlook.de>
2024-01-14 19:59:12 +0100

    add start-button

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -9,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class StartScreenComponent {
 
+
+  constructor(private router: Router){ }
+
+
+  newGame(){    
+    // Star game
+    this.router.navigateByUrl('/game');
+  }
+
+
 }
+
