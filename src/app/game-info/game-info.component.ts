f364a2f0bee973b301140e69b46b514776b33791
Stefan Jaroni <sjaroni@outlook.de>
2024-01-15 23:31:57 +0100

    improve game-infos after picking a card

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -6,8 +6,66 @@ import { MatCardModule } from '@angular/material/card';
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './game-info.component.html',
-  styleUrl: './game-info.component.scss'
+  styleUrl: './game-info.component.scss',
 })
-export class GameInfoComponent {
+export class GameInfoComponent implements OnChanges {
+  cardAction = [
+    {
+      title: 'Waterfall',
+      description:
+        'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.',
+    },
+    { title: 'You', description: 'You decide who drinks' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
+    {
+      title: 'Category',
+      description:
+        'Come up with a category (e.g. Colors). Each player must enumerate one item from the category.',
+    },
+    {
+      title: 'Bust a jive',
+      description:
+        'Player 1 makes a dance move. Player 2 repeats the dance move and adds a second one. ',
+    },
+    { title: 'Chicks', description: 'All girls drink.' },
+    {
+      title: 'Heaven',
+      description: 'Put your hands up! The last player drinks!',
+    },
+    {
+      title: 'Mate',
+      description:
+        'Pick a mate. Your mate must always drink when you drink and the other way around.',
+    },
+    {
+      title: 'Thumbmaster',
+      description:
+        'The player who drew the card must put their thumb on the table at a chosen time (before the next five gets picked though, or they lose the right). The last person to put their thumb on the table must drink.',
+    },
+    { title: 'Men', description: 'All men drink.' },
+    { title: 'Quizmaster', description: '' },
+    {
+      title: 'Never have i ever...',
+      description:
+        'Say something you nnever did. Everyone who did it has to drink.',
+    },
+    {
+      title: 'Rule',
+      description:
+        'Make a rule. Everyone needs to drink when he breaks the rule.',
+    },
+  ];
 
+  title: string | undefined = '';
+  description: string | undefined = '';
+
+  @Input() card: string | undefined = '';
+
+  ngOnChanges(): void {
+    if (this.card) {
+      let cardNumber = +this.card.split('_')[1];
+      this.title = this.cardAction[cardNumber - 1].title;
+      this.description = this.cardAction[cardNumber - 1].description;
+    }
+  }
 }
